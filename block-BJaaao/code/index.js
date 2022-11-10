57eb09fe2c5df92a552d49c79fead56c75b3ed82
lester castelino <lstrcstlno@gmail.com>
2022-11-10 17:50:42 +0530

    assignment complete

--- block-BJaaao/code/index.js
+++ block-BJaaao/code/index.js
@@ -1,45 +1,36 @@
-// Using alert, prompt, confirm and console
-
-/*
-
-1. Do the following using `alert` function. When you find any square bracket
- replace that to the value of the variable.
-
-*/
-
 let num1 = 21;
 let num2 = 32;
 
-/*
-- Create a variable named `language` and store the value of `JavaScript` in it
-- Alert message saying `I am learning [language]`
-- Alert `The value of num1 is [num1] and the value of num2 is [num2]`
-- Alert `The sum of [num1] and [num2] is [num1 + num1]`
-*/
+let language = "JavaScript"
+alert("I am learning JavaScript");
+alert("the value of num1 is 21 and the value of num2 is 32");
+alert("the sum of 21 and 32 is 21 + 32");
 
-/*
-2. Do the following using `alert`, `prompt` and `confirm` and `console`
+prompt("What is your name?");
+let userName = prompt("What is your name?");
 
-  - Using prompt accept the name of the user and store it a variable names `userName`
-  - Using prompt accept the profession of the user and store it a variable names`userProfession`
-  - Using confirm check if user is adult or not and store the value in `isAdult`
-  - Alert message `I am [userName] a [userProfession]`
-  - Log the message `I am [userName] a [userProfession]`
-  - Alet message `Username: [userName]
-    Adult: [isAdult]
-    Profession: [userProfession]` (it should be in three different lines)
-  - Log the message using console.log `Username: [userName]
-    Adult: [isAdult]
-    Profession: [userProfession]` (it should be in three different lines)
-*/
+prompt("what is your profession");
+let userProfession = prompt("what is your profession");
 
-/*
-. Addition using prompt
+confirm("Are you an adult");
+let isAdult = confirm("Are you an adult");
 
-  - Accept two number values form user and store them in `numA` and`numB`.
-  - Alert the sum of both numbers. [numA + numB]
-  - Alert [numA - numB]
-  - Alert [numA * numB]
-  - Log using console.log [numA - numB]
-  - Log using console.log [numA * numB]
-*/
+alert("i am userName a userProfession")
+
+console.log("i am userName a userProfession");
+
+alert("Username : userName");
+alert("Adult : isAdult");
+alert("Profession: userProfession");
+
+console.log("Username: userName");
+console.log("Adult: isAdult");
+console.log("Profession: userProfession");
+
+let numA = 25;
+let numB = 15;
+
+alert("numA - numB");
+alert("numA * numB");
+console.log(numA - numB);
+console.log(numA * numB);
