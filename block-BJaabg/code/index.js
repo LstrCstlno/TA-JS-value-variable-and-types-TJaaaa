6eb4fe87346a456b39a26b63572dec912fe31890
lester castelino <lstrcstlno@gmail.com>
2022-11-23 22:30:34 +0530

    assinment 3 complete

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -1,18 +1,48 @@
 /*
 🎖 Write a program to calculate the total price of your phone purchase. With these conditions
- * [ ] You will keep purchasing phones (hint: loop!) until you run out of bank balance.
- * [ ] You'll also buy accessories for each phone as long as your purchase amount is below your spending threshold.
- * [ ] After you've calculated your purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted like ($334.76).
+ * [323.96] You will keep purchasing phones (hint: loop!) until you run out of bank balance.
+ * [29.97] You'll also buy accessories for each phone as long as your purchase amount is below your spending threshold.
+ * [329.94 ] After you've calculated your purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted like ($334.76).
  * [ ] Finally, check the amount against your bank account balance to see if you can afford it or not.
 */
 
-const SPENDING_THRESHOLD = 200;
-const TAX_RATE = 0.08;
-const PHONE_PRICE = 99.99;
-const ACCESSORY_PRICE = 9.99;
-
-var bank_balance = 303.91;
-var amount = 0;
 // your code goes here
 
 // ⛑ Answer of the above will `$334.76`.
+
+// const SPENDING_THRESHOLD = 200;
+// const TAX_RATE = 0.08;
+// const PHONE_PRICE = 99.99;
+// const ACCESSORY_PRICE = 9.99;
+
+// var bank_balance = 303.91;
+// var amount = 0;
+
+var bank_balance = 303.91;
+const PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
+var amount = 0;
+const TAX_RATE = 0.08;
+
+for (let i = PHONE_PRICE; i < bank_balance; i += PHONE_PRICE) {
+    phoneTotal = i;
+}
+
+totalPhones = (phoneTotal / PHONE_PRICE);
+
+accessoryTotal = (totalPhones*ACCESSORY_PRICE);
+
+purchaseAmount = (accessoryTotal + phoneTotal);
+
+totalTax = (purchaseAmount*TAX_RATE);
+
+totalAmount = (purchaseAmount + totalTax);
+
+alert(`the calculated purchase amount is $${totalAmount}`);
+
+if (totalAmount<bank_balance) {
+    alert('You can afford the phone');
+}else{
+        alert('you cannot afford the phone');
+    }
+
